bb9d3aed7dd9caf6fef3e4dd2f80ad9ad26a0deb
griff і⊙ <346896+griffio@users.noreply.github.com>
2026-04-08 17:15:17 +0100

    module

---
+++ pgsearch-module/src/main/kotlin/griffio/PgSearchModule.kt
@@ -0,0 +1,62 @@
+package griffio
+
+import app.cash.sqldelight.dialect.api.IntermediateType
+import app.cash.sqldelight.dialect.api.PrimitiveType
+import app.cash.sqldelight.dialect.api.SqlDelightModule
+import app.cash.sqldelight.dialect.api.TypeResolver
+import app.cash.sqldelight.dialects.postgresql.PostgreSqlTypeResolver
+import app.cash.sqldelight.dialects.postgresql.grammar.PostgreSqlParser
+import app.cash.sqldelight.dialects.postgresql.grammar.PostgreSqlParserUtil
+import com.alecstrong.sql.psi.core.psi.SqlExpr
+import com.intellij.lang.parser.GeneratedParserUtilBase.Parser
+import griffio.grammar.PgSearchParser
+import griffio.grammar.PgSearchParserUtil
+import griffio.grammar.PgSearchParserUtil.extension_expr
+import griffio.grammar.PgSearchParserUtil.index_method
+import griffio.grammar.PgSearchParserUtil.storage_parameters
+import griffio.grammar.psi.PgSearchExtensionExpr
+
+class PgSearchModule : SqlDelightModule {
+    override fun typeResolver(parentResolver: TypeResolver): TypeResolver = PgSearchTypeResolver(parentResolver)
+
+    override fun setup() {
+        PgSearchParserUtil.reset()
+        PgSearchParserUtil.overridePostgreSqlParser()
+        // As the grammar doesn't support inheritance - override type_name manually to try inherited type_name
+        // Capture any existing overrides (e.g., from other PostgreSql Modules)
+        val previousTypeName = PostgreSqlParserUtil.type_name
+        val previousExtensionExpr = PostgreSqlParserUtil.extension_expr
+        val previousIndexMethod = PostgreSqlParserUtil.index_method
+        val previousStorageParameters = PostgreSqlParserUtil.storage_parameters
+        // etc
+        PostgreSqlParserUtil.extension_expr = Parser { psiBuilder, i ->
+            extension_expr?.parse(psiBuilder, i)
+                    ?: PgSearchParser.extension_expr_real(psiBuilder, i)
+                    || previousExtensionExpr?.parse(psiBuilder, i)
+                    ?: PostgreSqlParser.extension_expr_real(psiBuilder, i)
+        }
+        // etc
+        PostgreSqlParserUtil.index_method = Parser { psiBuilder, i ->
+            index_method?.parse(psiBuilder, i)
+                    ?: PgSearchParser.index_method_real(psiBuilder, i)
+                    || previousIndexMethod?.parse(psiBuilder, i)
+                    ?: PostgreSqlParser.index_method_real(psiBuilder, i)
+        }
+        // etc
+        PostgreSqlParserUtil.storage_parameters = Parser { psiBuilder, i ->
+            storage_parameters?.parse(psiBuilder, i)
+                    ?: PgSearchParser.storage_parameters_real(psiBuilder, i)
+                    || previousStorageParameters?.parse(psiBuilder, i)
+                    ?: PostgreSqlParser.storage_parameters_real(psiBuilder, i)
+        }
+    }
+}
+
+// Change to inheritance where some implementations may need to call `super` - not possible with delegation
+// parentResolver is called to delegate to the next TypeResolver in the chain
+private class PgSearchTypeResolver(private val parentResolver: TypeResolver) : PostgreSqlTypeResolver(parentResolver) {
+    override fun resolvedType(expr: SqlExpr) : IntermediateType {
+        return if (expr is PgSearchExtensionExpr && expr.proximityOperatorExpression != null)
+            IntermediateType(PrimitiveType.REAL) else parentResolver.resolvedType(expr) // use parentResolver to use the module chain
+    }
+}
---
+++ pgsearch-module/src/main/kotlin/griffio/grammar/mixins/PgSearchProximityOperatorMixin.kt
@@ -0,0 +1,20 @@
+package griffio.grammar.mixins
+
+import com.alecstrong.sql.psi.core.psi.SqlBinaryExpr
+import com.alecstrong.sql.psi.core.psi.SqlCompositeElementImpl
+import com.alecstrong.sql.psi.core.psi.SqlExpr
+import com.intellij.lang.ASTNode
+import griffio.grammar.psi.PgSearchProximityOperator
+
+/**
+ * Used for `x &&& y` expressions in SqlBinaryExpr type resolver
+ */
+internal abstract class PgSearchProximityOperatorMixin(node: ASTNode) :
+    SqlCompositeElementImpl(node),
+    SqlBinaryExpr,
+    PgSearchProximityOperator {
+
+    override fun getExprList(): List<SqlExpr> {
+        return children.filterIsInstance<SqlExpr>()
+    }
+}
